57475c067ba6420e9865f45434c39617904b26a4
GitJule <julia.bodnar@icloud.com>
2025-04-02 12:13:41 +0200

    licence header hinzugefügt

--- include/globaltypes.hpp
+++ include/globaltypes.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2006-2025 Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2025 Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: CC0-1.0
+
 #include <libspm/seqan/alphabet.hpp>
 #include <libjst/rcms/compressed_multisequence.hpp> 
 #include <libjst/rcms/rcs_store.hpp>

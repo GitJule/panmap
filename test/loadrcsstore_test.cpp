2f267b57aa336ea04bf422a175f03eaa5c46cc94
GitJule <julia.bodnar@icloud.com>
2025-04-27 14:47:43 +0200

    testdaten angepasst

--- test/loadrcsstore_test.cpp
+++ test/loadrcsstore_test.cpp
@@ -2,42 +2,24 @@
 // SPDX-FileCopyrightText: 2016-2025 Knut Reinert & MPI für molekulare Genetik
 // SPDX-License-Identifier: CC0-1.0
 
-// #include <gtest/gtest.h>
-// #include <loadjst.hpp>
-// #include <libspm/seqan/alphabet.hpp>
+#include <gtest/gtest.h>
+#include <loadjst.hpp>
+#include <libspm/seqan/alphabet.hpp>
 
-// TEST(test, create_rcs) {
-//     using namespace spm;
+TEST(test, create_rcs) {
+    using namespace spm;
 
-//     rcs_store_t rcs_store{"ACGTGACTGACTGACTAGCTACG"_dna5, 5};
-//    using variant_t = std::ranges::range_value_t<cms_t>;
-//    auto dom = rcs_store.variants().coverage_domain();
-//    rcs_store.add (variant_t{libjst::breakpoint{3,4},"C"_dna5, coverage_t{{0,2},dom}});
-//    EXPECT_EQ(rcs_store.size(), 5);   
-// }
-
-// TEST(test, loadrcsstore){
-//     auto rcsstorepath = std::filesystem::path{std::string{DATADIR}}.concat("test.jst");
-//     rcs_store_t store = loadjst(rcsstorepath);
-//     EXPECT_EQ(store.size(), 5);
-// }
-
-TEST(test, generator) {
-    auto store = loadjst("test.jst");
-    
-    generate_test_sequences( 
-        store,
-        "test_queries.fasta",
-        50,   // Patternlänge
-        1000, // Samples pro Knoten
-        1234  // Fester Seed
-    );
-    
-    seqan3::sequence_file_input fasta_in{"test_queries.fasta"};
-    size_t count = 0;
-    for (auto const& record : fasta_in) {
-        EXPECT_EQ(record.sequence().size(), 50); 
-        count++;
-    }
-    EXPECT_GE(count, 1000);
+    rcs_store_t rcs_store{"ACGTGACTGACTGACTAGCTACG"_dna5, 5};
+   using variant_t = std::ranges::range_value_t<cms_t>;
+   auto dom = rcs_store.variants().coverage_domain();
+   rcs_store.add (variant_t{libjst::breakpoint{3,4},"C"_dna5, coverage_t{{0,2},dom}});
+   EXPECT_EQ(rcs_store.size(), 5);   
 }
+
+TEST(test, loadrcsstore){
+    auto rcsstorepath = std::filesystem::path{std::string{DATADIR}}.concat("test.jst");
+    rcs_store_t store = loadjst(rcsstorepath);
+    EXPECT_EQ(store.size(), 5);
+}
+
+
